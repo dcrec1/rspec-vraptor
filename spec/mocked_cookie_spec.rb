d290b6b4ecd1898d4846b050eef6959b50dd2e9f
dcrec1 <dc.rec1@gmail.com>
2009-03-18 13:12:48 -0300

    removed spaces before argument parentheses

--- spec/mocked_cookie_spec.rb
+++ spec/mocked_cookie_spec.rb
@@ -11,32 +11,32 @@ describe MockedCookie do
 
   it "should set the path" do	  
     @cookie.set_path @path
-    @cookie.path.should eql (@path)
+    @cookie.path.should eql(@path)
   end
 	
   it "should set the MaxAge" do	 
     max_age = 100	  
     @cookie.set_max_age max_age
-    @cookie.max_age.should eql (max_age)
+    @cookie.max_age.should eql(max_age)
   end
 
   it "should set the value" do
      new_value = '654321'
      @cookie.set_value new_value
-     @cookie.value.should eql (new_value)
+     @cookie.value.should eql(new_value)
   end
 
   it "should get the name" do
-     @cookie.get_name.should eql (@name)
+     @cookie.get_name.should eql(@name)
   end
 
   it "should get the value" do
-     @cookie.get_value.should eql (@value)
+     @cookie.get_value.should eql(@value)
   end
 
   it "should get the path" do
      @cookie.path = @path	  
-     @cookie.get_path.should eql (@path)
+     @cookie.get_path.should eql(@path)
   end
 
 end
